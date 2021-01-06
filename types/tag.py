2fc34e4f0fd6f3f39a46a36d2c89e81a83940278
amindal <mohammadaminjafarian1375@gmail.com>
2021-01-06 19:56:42 +0330

    add tag functions in data base service

--- types/tag.py
+++ types/tag.py
@@ -18,8 +18,24 @@ class tag :
     
     def remove(self) : 
         self.__is_active = False
+
+    def get_id(self) : 
+        return self.__id
+
+    def get_name(self) : 
+        return self.__name
+
+    def get_is_active(self) : 
+        return self.__is_active
         
-        
+    def set_id(self, id) : 
+        self.__id = id
+
+    def set_name(self, name) : 
+        self.__name = name
+
+    def set_is_active(self, is_active) : 
+        self.__is_active = is_active
     
         
     
